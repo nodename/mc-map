10e222b0cc6e938393255e045cb939949088cf0a
Alan Shaw <alan@nodename.com>
2014-02-09 15:21:16 -0800

    This one works but recreates the map whenever the button is pushed

--- src/mc_map/core.cljs
+++ src/mc_map/core.cljs
@@ -7,10 +7,19 @@
 (defn log [& more]
   (.log js/console (apply str more)))
 
-(def SAN_FRANCISCO (google.maps.LatLng. 37.768544 -122.443514))
+(def SIXTEENTH_AND_MISSION (google.maps.LatLng. 37.764847 -122.420042))
+(def ALPHABET_CITY (google.maps.LatLng. 40.724545 -73.979050))
 
-(def app-state (atom {:mapOptions #js {:center SAN_FRANCISCO
+(def app-state (atom {:mapOptions #js {:center SIXTEENTH_AND_MISSION
                                        :zoom 14}}))
+
+(def *MAP* nil)
+
+(defn move-to-NY [app]
+  (log "move-to-NY")
+  (om/update! app assoc-in [:mapOptions] #js {:center ALPHABET_CITY :zoom 14})
+  ;(.panTo *MAP* ALPHABET_CITY)
+  )
 
 (defn map-view [app owner]
   (reify
@@ -18,14 +27,18 @@
     (render [this]
             (log "render" this)
             ;; render has to return a component:
-            (dom/div nil))
+            (dom/div nil "DIV"))
 
     om/IDidMount
     (did-mount [this node]
                (log "did-mount" (js->clj (:mapOptions app)))
                ;; node refers to the component I returned in render,
                ;; but I ignore it and attach the Map to its parentNode "map-canvas":
-               (google.maps.Map. (.-parentNode node) (:mapOptions app)))))
+               (let [the-map (google.maps.Map. (.-parentNode node) (:mapOptions app))]
+                 (set! *MAP* the-map))
+               (.addEventListener (. js/document (getElementById "map-button")) "click" (fn [e] (move-to-NY app)))
+               ;(dom/button #js {:width "20%" :float "left" :onClick #(move-to-NY app)} "Move")
+               )))
 
 (om/root
   app-state
