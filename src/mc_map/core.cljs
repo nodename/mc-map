0efa9d484ebb18b912d569d0c443ab62c96dea85
Alan Shaw <alan@nodename.com>
2014-02-09 22:41:11 -0800

    life cycle now handled properly

--- src/mc_map/core.cljs
+++ src/mc_map/core.cljs
@@ -1,6 +1,8 @@
 (ns mc-map.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]))
 
 (enable-console-print!)
 
@@ -11,37 +13,57 @@
 (def ALPHABET_CITY (google.maps.LatLng. 40.724545 -73.979050))
 
 (def app-state (atom {:mapOptions #js {:center SIXTEENTH_AND_MISSION
-                                       :zoom 14}}))
+                                       :zoom 14}
+                      :points []}))
 
-(def *MAP* nil)
+(defn marker-from-point [point gmap]
+  (google.Maps.Marker. #js {:position (google.Maps.LatLng (.latitude point) (.longitude point))
+                            :map gmap
+                            :title (.label point)}))
 
-(defn move-to-NY [app]
-  (log "move-to-NY")
-  (om/update! app assoc-in [:mapOptions] #js {:center ALPHABET_CITY :zoom 14})
-  ;(.panTo *MAP* ALPHABET_CITY)
-  )
+(defn update-markers [points {:keys [gmap markers] :as owner}]
+  (map markers #(.setMap % nil))
+  (let [new-markers (map #(marker-from-point % gmap) points)]
+    (om/set-state! owner :markers new-markers)))
 
 (defn map-view [app owner]
   (reify
-    om/IRender
-    (render [this]
-            (log "render" this)
-            ;; render has to return a component:
-            (dom/div nil "DIV"))
+    om/IInitState
+    (init-state [_]
+                {:move (chan)
+                 :gmap nil
+                 :markers []})
+
+    om/IWillMount
+    (will-mount [_]
+      ;; establish a go loop that will listen for move events and
+      ;; change the location in the app state using om/update! :
+      (let [move (om/get-state owner :move)]
+        (go (loop []
+          (let [loc (<! move)]
+            (om/update! app assoc-in [:mapOptions] {:center loc :zoom 14}))
+            (recur)))))
+
+    om/IRenderState
+    (render-state [this {:keys [move gmap]}]
+                  (when (and gmap (:center (:mapOptions app)))
+                    (.panTo gmap (:center (:mapOptions app))))
+
+                  (dom/div #js {:style #js {:width "100%" :height "100%"}}
+                           (dom/div #js {:id "map-holder" :style #js {:width "80%" :height "100%" :float "left"}})
+                           (dom/button #js {:width "20%" :float "left"
+                                            :onClick (fn [e] (put! move ALPHABET_CITY))}
+                                       "Move to New York")))
 
     om/IDidMount
     (did-mount [this node]
-               (log "did-mount" (js->clj (:mapOptions app)))
-               ;; node refers to the component I returned in render,
-               ;; but I ignore it and attach the Map to its parentNode "map-canvas":
-               (let [the-map (google.maps.Map. (.-parentNode node) (:mapOptions app))]
-                 (set! *MAP* the-map))
-               (.addEventListener (. js/document (getElementById "map-button")) "click" (fn [e] (move-to-NY app)))
-               ;(dom/button #js {:width "20%" :float "left" :onClick #(move-to-NY app)} "Move")
-               )))
+               (let [the-map (google.maps.Map. (. js/document (getElementById "map-holder"))
+                                               (:mapOptions app))]
+                 (om/set-state! owner :gmap the-map))
+               (update-markers (:points app) owner))))
 
 (om/root
   app-state
   map-view
-  (. js/document (getElementById "map-canvas")))
+  (. js/document (getElementById "content")))
 
