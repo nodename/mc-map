8aa8a53822cb24f65398ec7b267b3e189f743db6
Alan Shaw <alan@nodename.com>
2014-03-02 23:44:50 -0800

    removed react from :dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
                  [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.3.0"]
-                 [com.facebook/react "0.8.0.1"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
 
