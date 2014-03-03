81c2aaf1249ed2471ed6895390401bec07a3f558
Alan Shaw <alan@nodename.com>
2014-03-02 23:44:06 -0800

    updated cljs version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]
+                 [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.3.0"]
                  [com.facebook/react "0.8.0.1"]]
